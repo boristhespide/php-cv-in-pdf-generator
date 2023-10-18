69a42eb08fa361920d15738c079abc1d55bcc52b
boristhespide <skajitepochtu@gmail.com>
2023-10-18 10:29:16 +0800

    Перевод в ооп стиль

--- generate-pdf.php
+++ generate-pdf.php
@@ -1,32 +1,12 @@
 <?php
 
 require_once ("vendor/autoload.php");
-
+require_once ('Printer.php');
+//require_once ("Printer.php");
+//require_once ("Resume.php");
 use Dompdf\Dompdf;
 use Dompdf\Options;
 
-$name = htmlspecialchars($_POST["name"]);
-$position = htmlspecialchars($_POST["position"]);
-$about = htmlspecialchars($_POST["about"]);
-$gitLink = $_POST["gitLink"];
-$email = htmlspecialchars($_POST["email"]);
+new Printer();
 
 
-$options = new Options();
-$options->setChroot(__DIR__);
-$options->setIsRemoteEnabled(true);
-$options->set('defaultFont', 'DejaVu');
-
-$dompdf = new Dompdf($options);
-
-$dompdf->setPaper("A4", "landscape");
-
-$html = file_get_contents("template.html");
-
-$html = str_replace(["{{ name }}", "{{ position }}", "{{ about }}", "{{ gitLink }}", "{{ email }}"], [$name, $position, $about, $gitLink, $email], $html);
-
-$dompdf->loadHtml($html);
-
-$dompdf->render();
-
-$dompdf->stream('Резюме.pdf', ["Attachment" => 0]);
